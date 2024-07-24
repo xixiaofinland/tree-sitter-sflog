1b95333e62c406830d3ec80ac557d25eb5103e01
xi xiao <tdxiaoxi2@gmail.com>
2024-07-24 22:16:49 +0300

    init

---
+++ bindings/swift/TreeSitterSflog/sflog.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_SFLOG_H_
+#define TREE_SITTER_SFLOG_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_sflog(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_SFLOG_H_
---
+++ grammar.js
@@ -0,0 +1,7 @@
+module.exports = grammar({
+  name: 'sflog',
+
+  rules: {
+    source_file: $ => 'hello'
+  }
+});
